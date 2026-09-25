a5052e82063cf4fcb219ae4df43b0ba3862612e4
Ella-sly <sylviachiomafrancis@gmail.com>
2026-09-25 16:57:31 +0100

    feat:Add get_renewal_allowance view function

--- contracts/ahjoor-refund/src/lib.rs
+++ contracts/ahjoor-refund/src/lib.rs
@@ -6058,6 +6058,9 @@ mod test_cross_contract_refund;
 mod test_deadline_boundaries;
 
 #[cfg(test)]
+mod test_store_credit;
+
+#[cfg(test)]
 mod test_getters;
 /// Event: Customer appealed a rejected refund (#159)
 #[contractevent]
